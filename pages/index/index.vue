d33e2bd308e44ff836099b2427d686e469e8e558
胖橙 <3120154500@qq.com>
2023-03-08 15:36:41 +0800

    注册界面

---
+++ pages/index/index.vue
@@ -0,0 +1,44 @@
+<!-- ©️ 昶孝 版本更新网站 Author: SCC -->
+<template>
+	<view>
+		<view class="cx-top">
+			<image class="cx-logo" src="../../static/logo.png"></image>
+			<view class="cx-title">AppUpdateStore</view>
+		</view>
+		
+		<view class="cx-border">
+			<view class="cx-login-border">
+				<view class="cx-login-title">AppUpdateStore 版本管家</view>
+				<view class="cx-login-banquan">Copyright © 2018-2023 Tianjin LoadingCreate Technology Co., Ltd</view>
+				<view style="display: flex; justify-content: center;">
+					<view class="cx-input">
+						<input class="cx-input-id" @input="onKeyInput" placeholder="账号" />
+						<input class="cx-input-id" @input="onKeyInput" password="true" placeholder="密码" />
+						<button class="cx-login-btn">登陆</button>
+						<view class="cx-question">遇到问题?没有账号?</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	@import url(./index.css);
+</style>
